214e002f7f67475e40b01c0359fdad64d6f09eb5
Lucas18n <lucascurso3347@gmail.com>
2025-04-29 21:09:35 -0300

    m5

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,15 @@
-INSERT INTO tb_clientes (nome, cpf, email, telefone, cep) VALUES ('João da Silva','123.123.123-00', 'joao@email.com', '43999999999', '01001-000');
-INSERT INTO tb_clientes (nome, email, telefone) VALUES ('Maria Oliveira', 'maria@email.com', '43988888888');
-INSERT INTO tb_clientes (nome, email, telefone) VALUES ('Carlos Souza', 'carlos@email.com', '43977777777');
-INSERT INTO tb_clientes (nome, email, telefone) VALUES ('Ana Martins', 'ana@email.com', '43966666666');
+INSERT INTO tb_clientes (nome, cpf, email, telefone, cep) VALUES ('João da Silva', '123.123.123-00', 'joao@email.com', '(43) 99999-9999', '01001-000');
+INSERT INTO tb_clientes (nome, cpf, email, telefone, cep) VALUES ('Maria Oliveira', '111.123.123-00', 'maria@email.com', '(43) 98888-8888', '51312-020');
+INSERT INTO tb_clientes (nome, cpf, email, telefone, cep) VALUES ('Carlos Souza', '222.123.123-00', 'carlos@email.com', '(43) 97777-7777', '11201-028');
+INSERT INTO tb_clientes (nome, cpf, email, telefone, cep) VALUES ('Ana Martins', '888.123.123-00', 'ana@email.com', '(43) 96666-6666', '43578-954');
 
 
 INSERT INTO tb_produtos (nome, descricao, fabricacao, validade) VALUES ('Pão', 'pão gostoso', '2004-02-21', '2004-02-21');
 
-INSERT INTO tb_produtos (nome, descricao, fabricacao, validade) VALUES ('Bolo', 'pão gostoso', '2004-02-21', '2004-02-21');
+INSERT INTO tb_produtos (nome, descricao, fabricacao, validade) VALUES ('Bolo', 'bolo gostoso', '2004-02-21', '2004-02-21');
 
 INSERT INTO tb_produtos (nome, descricao, fabricacao, validade) VALUES ('Pão', 'pão ruim', '2004-02-21', '2004-02-21');
 
-INSERT INTO tb_produtos (nome, descricao, fabricacao, validade) VALUES ('Bolo', 'pão ruim', '2004-02-21', '2004-02-21');
+INSERT INTO tb_produtos (nome, descricao, fabricacao, validade) VALUES ('Bolo', 'bolo ruim', '2004-02-21', '2004-02-21');
 
 
